ff9d99a69aef72f283e32cfc8b6c56976503c656
GuasaPlay <gameplaysoscar@gmail.com>
2022-05-20 16:09:50 -0500

    Fixed url endpoint api

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const dojoApi = axios.create({
   // baseURL: "http://localhost:4000/api/v1",
-  baseURL: "http://ec2-3-224-28-91.compute-1.amazonaws.com:4000/api/v1",
+  baseURL: "https://codingdojo.ticsga.com/api/v1",
 });
 
 // dojoApi.interceptors.request.use(async (config) => {
